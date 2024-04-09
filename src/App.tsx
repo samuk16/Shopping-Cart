8b88787805569bd653388891b954436e16068106
Samuk <marcelolamas19@gmail.com>
2024-04-08 22:13:06 -0300

    Install shadcn/ui dependency for UI enhancements

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from '@/components/theme-provider'
+// import './styles/App.css'
 
+import { Button } from '@/components/ui/button'
+import { ModeToggle } from '@/components/mode-toggle'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"> 
+    <div className='h-screen w-full'>
+      <ModeToggle />
+      
+        <Button>test</Button>
+      
+    </div>
+    </ThemeProvider>
   )
 }
 
