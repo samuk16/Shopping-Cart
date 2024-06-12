79ed7ec6a8e93e9c4d7e9370aa543f0a15c7af8f
Samuk <marcelolamas19@gmail.com>
2024-06-11 23:27:36 -0300

    Improve Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,12 @@ import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import { Separator } from "@/components/ui/separator";
-import React from "react";
-import { cn } from "@/lib/utils";
+import defaultWeapons from "@/data/DefaultWeapons.json";
+import { ListItem } from "./ListItem";
 
 function Header() {
   return (
@@ -23,108 +22,75 @@ function Header() {
 
         <NavigationMenu>
           <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Pistols</NavigationMenuTrigger>
-              <NavigationMenuContent className="right:0 absolute left-auto top-full w-auto">
-                <ul className="grid w-[300px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[500px] ">
-                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((item) => (
-                    <ListItem href="/pistols/usp" key={item}>
-                      Usp
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Mid-tier</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[300px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[500px] ">
-                  <div>
-                    <p className="font-bold">SMG</p>
-                    <Separator />
-                    {[1, 2, 3, 4, 5, 6, 7].map((item) => (
-                      <ListItem
-                        href="/pistols/usp"
-                        title="Usp"
-                        key={item}
-                      ></ListItem>
-                    ))}
-                  </div>
-                  <div>
-                    <p className="font-bold">SHOTGUNS</p>
-                    <Separator />
-                    {[1, 2, 3, 4].map((item) => (
-                      <ListItem href="/pistols/usp" title="Usp" key={item}>
-                        Usp
-                      </ListItem>
-                    ))}
-                  </div>
-                  <div>
-                    <p className="font-bold">MACHINE GUNS</p>
-                    <Separator />
-                    {[1, 2].map((item) => (
-                      <ListItem href="/pistols/usp" title="Usp" key={item}>
-                        Usp
-                      </ListItem>
-                    ))}
-                  </div>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Rifles</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                {/* <ul className="grid w-[300px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[500px]"> */}
-                <ul className="grid w-[300px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[500px]">
-                  <div>
-                    <p className="font-bold">ASSAULT RIFLES</p>
-                    <Separator />
-                    {[1, 2, 3, 4, 5, 6, 7].map((item) => (
-                      <ListItem href="/pistols/usp" title="Usp" key={item}>
-                        Usp
-                      </ListItem>
-                    ))}
-                  </div>
-                  {/* <Separator orientation="vertical" decorative /> */}
-                  <div>
-                    <p className="font-bold">SNIPER RIFLES</p>
-                    <Separator />
-                    {[1, 2, 3, 4].map((item) => (
-                      <ListItem href="/pistols/usp" title="Usp" key={item}>
-                        Usp
-                      </ListItem>
-                    ))}
-                  </div>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Knives</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[300px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[500px] ">
-                  {[
-                    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
-                    18, 19, 20,
-                  ].map((item) => (
-                    <ListItem href="/pistols/usp" title="Usp" key={item}>
-                      Usp
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Gloves</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[200px] gap-3 p-4 md:w-[200px] lg:w-[250px] ">
-                  {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
-                    <ListItem href="/pistols/usp" title="Usp" key={item}>
-                      Usp
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+            {(defaultWeapons as DefaultWeapons).categories.map((obj, idx) => {
+              return (
+                <>
+                  {obj?.subCategory && (
+                    <NavigationMenuItem className="order-2">
+                      <NavigationMenuTrigger>
+                        {obj.subCategory}
+                      </NavigationMenuTrigger>
+                      <NavigationMenuContent className="w-auto">
+                        <div
+                          className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] p-4 gap-4 w-[500px] min-w-[200px]"
+                          key={idx}
+                        >
+                          {obj?.categories?.map((obj2) => {
+                            return (
+                              <div className="flex flex-col gap-2">
+                                <p className="font-bold">{obj2.name}</p>
+                                <Separator />
+                                {obj2?.items &&
+                                  obj2.items.map((item, idx) => (
+                                    <ListItem
+                                      href="/pistols/usp"
+                                      title={item.name}
+                                      key={`${obj.category}-${idx}`}
+                                      imgSrc={item.skinUrl}
+                                    ></ListItem>
+                                  ))}
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </NavigationMenuContent>
+                    </NavigationMenuItem>
+                  )}
+                </>
+              );
+            })}
+            {(defaultWeapons as DefaultWeapons).categories.map((obj) => {
+              return (
+                <>
+                  {obj?.category && (
+                    <NavigationMenuItem
+                      className={
+                        obj.category === "Pistols" ? "order-1" : "order-3"
+                      }
+                    >
+                      <NavigationMenuTrigger>
+                        {obj.category}
+                      </NavigationMenuTrigger>
+                      <NavigationMenuContent className="right:0 absolute left-auto top-full w-auto">
+                        <div className="grid w-[300px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[500px]">
+                          {obj.items?.map((item, idx) => {
+                            return (
+                              <ListItem
+                                imgSrc={item.skinUrl}
+                                href="/pistols/usp"
+                                key={`${obj.category}-${idx}`}
+                              >
+                                {item.name}
+                              </ListItem>
+                            );
+                          })}
+                        </div>
+                      </NavigationMenuContent>
+                    </NavigationMenuItem>
+                  )}
+                </>
+              );
+            })}
           </NavigationMenuList>
         </NavigationMenu>
       </div>
@@ -149,29 +115,5 @@ function Header() {
     </header>
   );
 }
-const imgtest =
-  "https://cdn.csgoskins.gg/public/uih/weapons/aHR0cHM6Ly9jZG4uY3Nnb3NraW5zLmdnL3B1YmxpYy9pbWFnZXMvYnVja2V0cy9lY29uL3dlYXBvbnMvYmFzZV93ZWFwb25zL3dlYXBvbl9jejc1YS5mY2NlZGI5OTJhODZjYzM0MTUwNDZjMzAzZDRlYTU2NjEwM2Q4ZDYyLnBuZw--/50/auto/85/notrim/181ec9fe16f282b3dc1ce3f7757656a9.webp";
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "flex items-center select-none rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className,
-          )}
-          {...props}
-        >
-          <img src={imgtest} alt="skinAk47" />
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="">{children}</p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
+
 export default Header;
